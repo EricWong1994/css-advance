15683adb8146469404faf4677f8c7515ca8266a7
搬砖达人小王git-flow <461864051@qq.com>
2022-08-21 22:14:53 +0800

    有问题

---
+++ src/components/dem/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div className="dem-component">Dem Component {{done}}</div>
+</template>
+
+<style lang="scss">
+.dem-component {
+	font-size: 20px;
+	color: #f66;
+}
+</style>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "dem",
+	props: {
+		done: {
+			default: "Done",
+			type: String
+		}
+	}
+});
+</script>
--- src/views/home/components/juejin/index.vue
+++ src/views/home/components/juejin/index.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="juejin-view">
 		<div class="container">掘金CSS艺术之美</div>
+		<div>
+			<button ref="red-theme-bt">切换红色主题</button>
+			<button ref="blue-theme-bt">切换蓝色主题</button>
+			<button ref="green-theme-bt">切换绿色主题</button>
+		</div>
 	</div>
 </template>
 
@@ -10,5 +15,22 @@ import './index.less';
 
 export default defineComponent({
 	name: 'juejin',
+	data() {
+		a: 1;
+	},
+	mounted() {
+		debugger;
+		console.log(this.a);
+		console.log(1);
+		[('red', 'blue', 'green')].forEach(v => {
+			console.log('v: ', v);
+			// const btn = document.getElementById(`${v}-theme-btn`);
+			// console.log('btn: ', btn);
+			// this.$refs[v].addEventListener('click', () => {
+			// 	console.log('click: ');
+			// 	document.body.style.setProperty('--bg-color', v);
+			// });
+		});
+	},
 });
 </script>
